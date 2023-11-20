56c686444c6fe43388813d2ba3d7c030aeea01f7
akamfoad <akamfanaye@gmail.com>
2023-11-20 16:50:11 +0300

    add: docs for ErrorCorrectionLevel

--- src/lib/ErrorCorrectLevel.ts
+++ src/lib/ErrorCorrectLevel.ts
@@ -1,6 +1,18 @@
 export const ErrorCorrectLevel = {
-  L: 1,
-  M: 0,
-  Q: 3,
-  H: 2,
+  /**
+   * Allows recovery of up to 7% data loss
+   */
+  L: 1 as const,
+  /**
+   * Allows recovery of up to 15% data loss
+   */
+  M: 0 as const,
+  /**
+   * Allows recovery of up to 25% data loss
+   */
+  Q: 3 as const,
+  /**
+   * Allows recovery of up to 30% data loss
+   */
+  H: 2 as const,
 };
